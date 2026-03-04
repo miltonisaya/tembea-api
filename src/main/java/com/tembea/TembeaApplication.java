754f729aa5b41f5444fa634f0db6dd192e3bbb16
Milton Isaya <miltonisaiah@gmail.com>
2026-03-04 12:07:12 +0300

    initial commit

---
+++ src/main/java/com/tembea/TembeaApplication.java
@@ -0,0 +1,13 @@
+package com.tembea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TembeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TembeaApplication.class, args);
+	}
+
+}
