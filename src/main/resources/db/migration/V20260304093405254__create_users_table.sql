b73b98e56f738f595d1bec4964ffd9d7354aa117
Milton Isaya <miltonisaiah@gmail.com>
2026-03-04 12:59:46 +0300

    created migration to create users

---
+++ src/main/resources/db/migration/V20260304093405254__create_users_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE users
+(
+    id          BIGSERIAL             NOT NULL,
+    uuid        VARCHAR(36)           NOT NULL,
+    first_name  VARCHAR(255)          NULL,
+    middle_name VARCHAR(255)          NULL,
+    last_name   VARCHAR(255)          NULL,
+    mobile      VARCHAR(255)          NULL,
+    sex         VARCHAR(255)          NULL,
+    user_type   VARCHAR(255)          NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
+
+ALTER TABLE users
+    ADD CONSTRAINT uc_users_uuid UNIQUE (uuid);
