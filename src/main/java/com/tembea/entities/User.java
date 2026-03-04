4262fc693c01d6a3280693d1ead38d52d68e3212
Milton Isaya <miltonisaiah@gmail.com>
2026-03-04 12:26:31 +0300

    created user entity

--- src/main/java/com/tembea/entities/User.java
+++ src/main/java/com/tembea/entities/User.java
@@ -1,4 +1,30 @@
 package com.tembea.entities;
 
+import com.tembea.enums.SexEnum;
+import com.tembea.enums.UserTypeEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
 public class User {
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "middle_name")
+  private String middleName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "mobile")
+  private String mobile;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "sex")
+  private SexEnum sex;
+
+  @Column(name = "user_type")
+  @Enumerated(EnumType.STRING)
+  private UserTypeEnum userType;
 }
