f88fd0d2981fe4976c699dc02e90b90ba90edb38
Milton Isaya <miltonisaiah@gmail.com>
2026-03-09 16:40:18 +0300

    removed unused imports

--- src/main/java/com/tembea/config/CorsConfig.java
+++ src/main/java/com/tembea/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.tembea.config;
 
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
